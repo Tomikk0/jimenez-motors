2b1369eb4d7c47d57d15d483695ba6fb4014471f
Tomikk0 <ujvarit27@gmail.com>
2025-10-24 06:19:40 +0200

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,3 +1,9 @@
+// === SUPABASE KAPCSOLAT === 
+const supabaseUrl = 'https://abpmluenermqghrrtjhq.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFicG1sdWVuZXJtcWdocnJ0amhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA3MTYzMjgsImV4cCI6MjA3NjI5MjMyOH0.YkTZME_BB86r3mM8AyNYu-2yaMdh4LtDhHbynvdkaKA';
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+
+// Globális változók
 let tuningOptions = [];
 let modelOptions = [];
 let tagOptions = [];
