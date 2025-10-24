4a2f14e3c29adbe75943f5af4bb35bdd234400ef
Tomikk0 <ujvarit27@gmail.com>
2025-10-24 06:12:52 +0200

    Delete api/api directory

--- api/api/getData.js
+++
@@ -1,4 +0,0 @@
-export default async function handler(req, res) {
-  const dbUrl = process.env.DATABASE_URL; // ez Vercel environmentből jön
-  res.status(200).json({ ok: true, message: "Hello Vercel API!" });
-}
