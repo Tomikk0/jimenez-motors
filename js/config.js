a19bb691d4d8fa955ea20bc68a79dbeb915e5cb5
Tomikk0 <ujvarit27@gmail.com>
2025-10-24 05:47:05 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,7 @@
-// config.js
-const supabaseUrl = window.__SUPABASE_URL__;
-const supabaseKey = window.__SUPABASE_KEY__;
+// === SUPABASE KAPCSOLAT === 
+const supabaseUrl = 'https://abpmluenermqghrrtjhq.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFicG1sdWVuZXJtcWdocnJ0amhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA3MTYzMjgsImV4cCI6MjA3NjI5MjMyOH0.YkTZME_BB86r3mM8AyNYu-2yaMdh4LtDhHbynvdkaKA';
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
-
 
 // Globális változók
 let tuningOptions = [];
@@ -14,6 +13,4 @@ let searchTimeout;
 let selectedImage = null;
 let currentCarIdForSale = null;
 let currentKickMemberName = null;
-
 let gallerySelectedImage = null;
-
